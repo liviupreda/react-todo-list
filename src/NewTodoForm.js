453831ea15cef279f1cf3d155c0760536e826084
Liviu Preda <liviupreda@gmail.com>
2019-11-11 14:23:33 +0200

    add TodoForm component, add todos to list

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,1 +1,43 @@
 import React, { Component } from 'react';
+
+class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createTodo(this.state);
+    this.setState({ task: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='task'>Enter task: </label>
+        <input
+          type='text'
+          placeholder='New Todo'
+          name='task'
+          id='task'
+          value={this.state.task}
+          onChange={this.handleChange}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default NewTodoForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
+import NewTodoForm from './NewTodoForm';
 
 class TodoList extends Component {
   constructor(props) {
@@ -7,6 +8,13 @@ class TodoList extends Component {
     this.state = {
       todos: [{ task: 'finish Todo app' }, { task: 'talk to the horse' }]
     };
+    this.create = this.create.bind(this);
+  }
+
+  create(newTodo) {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
   }
 
   render() {
@@ -16,6 +24,7 @@ class TodoList extends Component {
     return (
       <div>
         <h1>Todo List</h1>
+        <NewTodoForm createTodo={this.create} />
         <ul>{todos}</ul>
       </div>
     );
