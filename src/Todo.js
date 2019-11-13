065f9686ae1f484b313a351f1cdbfa4038794bce
Liviu Preda <liviupreda@gmail.com>
2019-11-13 10:39:38 +0200

    update Todo edit form state on change

--- src/Todo.js
+++ src/Todo.js
@@ -4,28 +4,54 @@ class Todo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isEditing: false
+      isEditing: false,
+      task: this.props.task
     };
     this.handleRemove = this.handleRemove.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
 
   handleRemove() {
     this.props.removeTodo(this.props.id);
   }
+
+  toggleForm() {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+
+  handleChange(e) {
+    this.setState({
+      task: e.target.value
+    });
+  }
+
+  handleUpdate(e) {
+    e.preventDefault();
+    // Take new task data and pass it up to parent
+  }
+
   render() {
     let result;
     if (this.state.isEditing) {
       result = (
         <div>
-          <form>
-            <input type="text" />
+          <form onSubmit={this.handleUpdate}>
+            <input
+              type="text"
+              value={this.state.task}
+              name="task"
+              onChange={this.handleChange}
+            />
+            <button>Save</button>
           </form>
         </div>
       );
     } else {
       result = (
         <div>
-          <button>Edit</button>
+          <button onClick={this.toggleForm}>Edit</button>
           <button onClick={this.handleRemove}>X</button>
           <li>{this.props.task}</li>
         </div>
